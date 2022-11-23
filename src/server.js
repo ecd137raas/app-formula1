22a9049de896bb833ee5810e674351d23fbb34a3
Emerson Duarte <raasjava@gmail.com>
2022-11-23 20:07:21 -0300

    feat: improvements in the code

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-  
+  console.log(`App listening on port ${port}`)
+})
+
