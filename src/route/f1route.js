9ec092f90064fd7baae7916268ca4ee1e334f3b2
Emerson Duarte <raasjava@gmail.com>
2023-03-22 19:22:10 -0300

    Atualizações

--- src/route/f1route.js
+++ src/route/f1route.js
@@ -7,20 +7,16 @@ router.get('/', async function (req, res) {
     const response = await service.getSchedule()
     res.render('index', { res: response })
 })
-
 router.get('/results', async function (req, res) {
     const response = await service.getResults()
     res.render('results', { res: response })
 })
-
 router.get('/constructors', async function (req, res) {
     const response = await service.getConstructors()
     res.render('constructors', { res: response })
 })
-
 router.get('/driverstandings', async function (req, res) {
     const response = await service.getDriverStandings()
     res.render('drivers', { res: response })
 })
-
 module.exports = router
